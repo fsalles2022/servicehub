9918a5e862b92aaf38d4a23a412f22ed7710554d
Fabio Salles <fabiolfs761@gmail.com>
2026-02-01 18:18:56 -0300

    feat - Update DatabaseSeeder to create an Admin user with default credentials

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         //User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin ServiceHub',
+            'email' => 'admin@servicehub.com',
+            'password' => bcrypt('admin12345')  // senha padrão
         ]);
 
         $this->call([
